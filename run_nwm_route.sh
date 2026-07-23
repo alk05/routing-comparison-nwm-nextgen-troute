958065c31369b2ef8656739305bb6b12865f0e2f
Quinn Lee <qylee@ua.edu>
2026-07-23 13:23:29 -0500

    added functionality to fill out troute file

--- run_nwm_route.sh
+++ run_nwm_route.sh
@@ -20,6 +20,7 @@
 #
 # Usage:
 #   bash run_nwm_route.sh --START_TIME 202607231200 --FORECAST_TYPE 1 --VPU 03W
+#       --N_CPUS 4
 # =============================================================================
 
 # Argument parser
@@ -27,6 +28,7 @@
 START_TIME=""
 FORECAST_TYPE=""
 VPU=""
+N_CPUS=""
 
 # Valid options
 VALID_FORECAST_TYPES="1 2 3 4 11"
@@ -47,6 +49,10 @@ while [[ $# -gt 0 ]]; do
             VPU="$2"
             shift 2
             ;;
+        --N_CPUS)
+            N_CPUS="$2"
+            shift 2
+            ;;
         --help)
             echo "Usage: $0 --START_TIME <YYYYMMDDHHmm> --FORECAST_TYPE <1|2|3|4|11> --VPU <VPU_code>"
             echo ""
@@ -54,8 +60,9 @@ while [[ $# -gt 0 ]]; do
             echo "  --START_TIME      Start time in YYYYMMDDHHmm format (required)"
             echo "  --FORECAST_TYPE   Forecast type: 1, 2, 3, 4, or 11 (required)"
             echo "  --VPU             VPU code: 01, 02, 03N, 03S, 03W, etc. (required)"
+            echo "  --N_CPUS          Number of CPUs to use in t-route"
             echo ""
-            echo "Example: $0 --START_TIME 202607031100 --FORECAST_TYPE 1 --VPU 03W"
+            echo "Example: $0 --START_TIME 202607031100 --FORECAST_TYPE 1 --VPU 03W -N_CPUS 4"
             exit 0
             ;;
         *)
@@ -108,17 +115,19 @@ mkdir ./restart
 wget -P ./restart -i filenamelist.txt -O analysis_assim.nc
 RESTART_FILE=$(python restart.py \
     --nwm_file_path ./restart/analysis_assim.nc \
-    --routelink_file_path RouteLink_CONUS.nc \
+    --routelink_file_path ./domain/RouteLink_CONUS.nc \
     --map_file_path nwm_to_ngen_map.json \
     --output_directory ./restart)
 
-# Some function to edit troute.yaml and run t-route
-# Do some regex magic to change start_datetime and nts, and mask file
-mkdir ./outputs
-# TODO: figure out regex
-# python3 -m nwm_routing -f -V4 troute.yaml
+# Edit troute.yaml and run t-route
+mkdir ./output
+envsubst < troute_template.yaml > troute.yaml
+python3 -m nwm_routing -f -V4 troute.yaml
+# TODO: make sure all env vars have values
+# MASK_FILE_PATH is derived from VPU.
+# NTS = MAX_LOOP_SIZE defined by RUNINPUT
 
 # Some function to convert NWM t-route outputs into NextGen catchment resolution
 python nwm_to_ngen.py \
     --nwm_to_ngen_map nwm_to_ngen_map.json \
-    --troute_outputs ./outputs
+    --troute_outputs ./output
